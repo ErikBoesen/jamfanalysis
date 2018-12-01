8e669518ec1a9869b9baaac9374fbfc0028a70ff
ErikBoesen <me@erikboesen.com>
2018-12-01 18:54:55 -0500

    Add plist reading code

--- analyze.py
+++ analyze.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import xml.etree.ElementTree
+import plistlib
 from datetime import date
 import os
+from pprint import pprint
 
 DATE = date.today().strftime('%Y-%m-%d')
 DIR = '/Library/Application Support/JAMF/Usage/' + DATE + '/'
@@ -9,7 +10,19 @@ DIR = 'Usage/' + DATE + '/'
 
 users = os.listdir(DIR)
 users = [user for user in users if user not in ['idle.plist']]
+USERNAME = 'boesene'
 
+with open(DIR + 'idle.plist', 'rb') as f:
+    idle = plistlib.load(f, fmt=plistlib.FMT_XML)
+pprint(idle)
+with open(DIR + USERNAME + '.plist', 'rb') as f:
+    user = plistlib.load(f, fmt=plistlib.FMT_XML)
+pprint(user)
+with open(DIR + '(null).plist', 'rb') as f:
+    null = plistlib.load(f, fmt=plistlib.FMT_XML)
+pprint(null)
+
+"""
 for user in users:
     # Usage logs are technically plist files, but are in XML format.
     tree = xml.etree.ElementTree.parse(DIR + user)
@@ -20,3 +33,4 @@ for user in users:
     for entry in main.findall('dict'):
         for field in entry.findall('string'):
             field.text = 3
+            """
